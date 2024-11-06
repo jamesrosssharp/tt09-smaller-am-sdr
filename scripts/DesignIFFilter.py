28205c6dd2aef1ad852086d55f50b9c93fddfd3b
James Sharp <jamesrosssharp@gmail.com>
2024-11-06 18:32:49 +1100

    Code simulating correctly, test github actions

--- scripts/DesignIFFilter.py
+++ scripts/DesignIFFilter.py
@@ -1,19 +1,29 @@
 from scipy import signal
 from matplotlib import pyplot as plt
 import numpy as np
+import cmath
+import math
 
-b, a = signal.iirfilter(1, [450000.0 / 25000000.0, 460000.0 / 25000000.0], btype='bandpass',
-                       analog=False, ftype='butter',
-                       output='ba'
-                       )
+#b, a = signal.iirfilter(1, [450000.0 / 25000000.0, 460000.0 / 25000000.0], btype='bandpass',
+#                       analog=False, ftype='butter',
+#                       output='ba'
+#                       )
+
+r = 0.995
+w0 = 0.057
+
+a = np.array([ 1, -2.0*r*math.cos(w0),  r*r ])
+b = np.array([ 0.005    , 0  ,  -0.005])
+
+
 
 scale_val = 8192.0
 
 print(a)
 print(b)
 
-a = np.round(a*scale_val)
-b = np.round(b*scale_val)
+a = np.floor(a*scale_val)
+b = np.floor(b*scale_val)
 
 print(a)
 print(b)
@@ -63,3 +73,33 @@ ax.set_ylabel('Amplitude [dB]')
 ax.grid(which='both', axis='both')
 
 plt.show()
+
+# Plot poles and zeros
+
+z, p, k = signal.tf2zpk(b, a)
+
+print(z)
+print(p)
+print(k)
+
+fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
+
+z1r,z1t = cmath.polar(z[0])
+z2r,z2t = cmath.polar(z[1])
+
+p1r,p1t = cmath.polar(p[0])
+p2r,p2t = cmath.polar(p[1])
+
+ax.plot(z1t, z1r, 'ko')
+ax.plot(z2t, z2r, 'ko')
+ax.plot(p1t, p1r, 'kx')
+ax.plot(p2t, p2r, 'kx')
+
+ax.set_rmax(1.1)
+ax.set_rticks([0.5, 1])  # Less radial ticks
+ax.set_rlabel_position(-22.5)  # Move radial labels away from plotted line
+ax.grid(True)
+
+ax.set_title("A line plot on a polar axis", va='bottom')
+plt.show()
+
---
+++ src/if_filter.v
@@ -0,0 +1,85 @@
+/* vim: set et ts=4 sw=4: */
+
+/*
+
+    Tiny AM SDR
+
+if_filter.v: 455kHz IF filter
+
+Copyright 2023 J.R.Sharp
+
+*/
+
+module if_filter
+(
+    input clk,
+    input RSTb,
+    input signed [7:0]  if_out,
+    output reg signed [7:0] if_filt_out,
+    input [2:0] gain_spi
+);
+
+//a = np.array([  8192., -16276.,   8110.])
+//b = np.array([ 40.,   0., -41.])
+/*   8192 * yn = 16347 yn-1 - 8182 yn-2 + 5 xn - 5 xn-2 */                
+
+reg signed [20:0] yn_1, yn_2;
+reg signed [7:0] xn_0, xn_1, xn_2;
+
+
+wire signed [15:0] a1 = -16'd16276;
+wire signed [15:0] a2 = 16'd8110;
+
+wire signed [15:0] b0 = 16'd5;
+wire signed [15:0] b2 = -16'd5;
+
+wire signed [36:0] mul_a1 = a1 * yn_1;
+wire signed [36:0] mul_a2 = a2 * yn_2;
+
+wire signed [23:0] mul_b0 = b0 * xn_0;
+wire signed [23:0] mul_b2 = b2 * xn_2;
+
+
+wire signed [25:0] sum = mul_b0 + mul_b2 - mul_a1[36:13] - mul_a2[36:13];
+
+wire signed [20:0] sum_out = sum[20:0];
+
+always @(posedge clk)
+begin
+    if (RSTb == 1'b0) begin
+
+        if_filt_out <= 8'd0;
+        yn_1 <= 21'd0;
+        yn_2 <= 21'd0;
+        xn_0 <= 8'd0;
+        xn_1 <= 8'd0;
+        xn_2 <= 8'd0;
+
+
+    end else begin
+
+        xn_0 <= if_out;
+        xn_1 <= xn_0;
+        xn_2 <= xn_1;
+
+        case (gain_spi)
+            3'd0:
+                 if_filt_out <= sum_out[20:13];
+            3'd1:
+                 if_filt_out <= sum_out[19:12];
+            3'd2:
+                 if_filt_out <= sum_out[18:11];
+            3'd3:
+                 if_filt_out <= sum_out[17:10];
+            3'd4:
+                 if_filt_out <= sum_out[16:9];
+            default:
+                 if_filt_out <= sum_out[15:8];
+        endcase
+
+        yn_1 <= sum_out;
+        yn_2 <= yn_1;
+    end 
+end
+
+endmodule
